e65ae564313611e84a094216b44df4b633ef3016
Alex Liao <gradop09@gmail.com>
2019-07-04 16:08:27 +0800

    Add new solution for 'Reverse Words In A String'

--- Reverse_Words_In_A_String/Main.cpp
+++ Reverse_Words_In_A_String/Main.cpp
@@ -10,6 +10,9 @@
 
 using namespace std;
 
+// This solution uses way too much memory and is too slow
+// Runtime: 20ms (faster than 25% of C++ submissions)
+// Memory: 133.7 MB (less than 5% of C++ submissions)
 class Solution {
 public:
 	string reverseWords(string s) {
@@ -74,8 +77,68 @@ public:
 	}
 };
 
-int main(void) {
-	string s = " ";
+// Solution 2: Improve on previous solution
+// * Without creating too many new string objects
+// * Reverse the words by extracting chars in reverse order from reversed string
+// Runtime: 8ms (90% faster than other C++ submissions)
+// Mem: 10 MB (less than 65.8% of other C++ submissions)
+class Solution2 {
+public:
+	string reverseWords(string s) {
+		int len = s.length();
+		string res = "";
 
-	return 0;
-}
+		if (len) {
+			// reverse the input string first
+			reverse(s.begin(), s.end());
+
+			int i = 0;
+			bool first_word = true;
+			while (i < len) {
+				// skip any leading (or could be trailing) white-space chars
+				while (s[i] == ' ') {
+					i++;
+					if (i == len) {
+						break;
+					}
+				}
+
+				// if i == len, either the string was all white-space chars, or we were skipping
+				// trailing spaces before and now we've reached end of string
+				if (i == len) {
+					break;
+				}
+
+				// otherwise, i is pointing to the beginning of a word, make j point to that too
+				int j = i;
+
+				// keep skipping until we find the next white-space char (word delimiter) or reach end of string
+				while (s[i] != ' ' && i != len) {
+					i++;
+				}
+
+				// now i points to the delimiter, and make k point to end of a word
+				int k = i - 1;
+				
+				// if we're not working on the first word, need to append a space char first
+				if (!first_word) {
+					res += ' ';
+				}
+
+				// reverse a word by extracting chars from the end of the word
+				// and append to output string
+				while (k >= j) {
+					res += s[k];
+					k--;
+				}
+
+				// after we've encountered the first word, the flag needs to be set to false
+				if (first_word) {
+					first_word = false;
+				}
+			}
+		}
+
+		return res;
+	}
+};
