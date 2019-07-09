dad53fe7398ac1d701f484b83956459b7778d85d
Alex Liao <gradop09@gmail.com>
2019-07-09 17:16:25 +0800

    Add new solution for 'Merge Sorted Array'

--- Merge_Sorted_Array/Main.cpp
+++ Merge_Sorted_Array/Main.cpp
@@ -7,13 +7,19 @@
 // clear nums1 and insert elements from result vector to nums1.
 
 // Method 2 (Refer to Grandyang):
-//
+// Working backwards from the end of :
+// 1. the merged array (end of nums1) 
+// 2. the end of valid data in nums1
+// 3. the end of nums2
+// we can insert the elements from nums1 and nums2 in order into the back of nums1
+// until we reach the 0th index in nums1 and nums2
 
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
+// Solution 1
 class Solution {
 public:
 	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -58,6 +64,46 @@ public:
 		nums1.clear();
 		// copy the sorted vector to nums1
 		nums1 = sorted;
+	}
+};
+
+// Solution 2
+class Solution2 {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		// find the end of valid datum in nums1 and nums2
+		int i = m - 1;
+		int j = n - 1;
+
+		// find the end of the merged array
+		int k = (m + n) - 1;
+
+		while (j >= 0) {
+			// if there are still elements in nums1 to be compared
+			if (i >= 0) {
+				if (nums1[i] > nums2[j]) {
+					nums1[k] = nums1[i];
+					i--;
+				}
+				else {
+					nums1[k] = nums2[j];
+					j--;
+				}
+				k--;
+				continue;
+			}
+
+			// if there are still remaining elements in nums2, insert them
+			if (j >= 0) {
+				nums1[k] = nums2[j];
+				j--;
+				k--;
+			}
+		}
+		
+		// there's nothing to do after getting out of the while loop, because if there
+		// are more elements left in nums1, they would be in the correct order and at
+		// the correct indices
 	}
 };
 
