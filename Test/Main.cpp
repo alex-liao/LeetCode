4ed09cab624741a81a063f7ecfb2881a0f56237e
Alex Liao <gradop09@gmail.com>
2019-08-15 17:14:02 +0800

    Set project 'Test' to an empty template

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -8,11 +8,5 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	BinaryTreeHelper* bt_helper = new BinaryTreeHelper();
-
-	vector<string> input = {"1"};
-
-	TreeNode* root = bt_helper->buildTree(input);
-
 	return 0;
 }
