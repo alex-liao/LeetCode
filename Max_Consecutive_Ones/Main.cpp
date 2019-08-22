cca1ef559d84988125e8e5057d5a95985155e32d
Alex Liao <gradop09@gmail.com>
2019-08-22 19:53:27 +0800

    Add algorithm explaination to 'Max_Consecutive_Ones'

--- Max_Consecutive_Ones/Main.cpp
+++ Max_Consecutive_Ones/Main.cpp
@@ -1,7 +1,15 @@
 // Problem: Max Consecutive Ones
 
 // Algorithm 1 (my own solution):
-// TBD
+// Use two variables, 'count' and 'max' to keep track of:
+//     The number of 1s in a sequence of consecutive 1s
+//     The maximum number of consecutive 1s we'v come across so far
+// Then iterate through the input vector, counting the number of consecutive 1s as we go.
+// When we encounter a 1, we increment count, when we encounter a 0, we check if current count
+// is the new max, and update max if needed then reset count to 0.
+// Finally, when the loop terminates, we check if current count is the new max again. This is because
+// if the vector was ending with a group of ones, without this check, we would miss count the last group
+// of consecutive 1s and it may be the max.
 
 #include <vector>
 
