783e99b6d73ef1e15f0ba879927f616756c95c25
Alex Liao <gradop09@gmail.com>
2019-11-25 23:46:41 +0800

    Add Python3 solution for 'Count Primes'

---
+++ To_Lower_Case/Main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
+    
+if __name__ == "__main__":
+    soln = Solution().toLowerCase("HELLO")
+    print(soln)
