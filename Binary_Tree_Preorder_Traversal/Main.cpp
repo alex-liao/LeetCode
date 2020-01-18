1dea1de8f0793a9c66068687c37c588e81df5af4
Alex Liao <gradop09@gmail.com>
2020-01-18 23:48:13 +0800

    Update iterative solution to Binary Tree Pre-Order Traversal

--- Binary_Tree_Preorder_Traversal/Main.cpp
+++ Binary_Tree_Preorder_Traversal/Main.cpp
@@ -6,9 +6,17 @@
 // root's left and right children nodes
 
 // Algorithm 2 (Recurisve solution):
-// TBD
+// Ref: https://www.geeksforgeeks.org/iterative-preorder-traversal/
+// We need an explicit stack in order to convert the recursive algorithm into iterative process
+// The iterative process goes like this:
+// 1. While the stack is not empty, we pop the stack and store the popped node's value into the output vector
+// 2. In order to visit the left child of a node before the right child node, we need to push the 
+//    right child into the stack first (if it exists).
+// 3. Then we push the left child node of the popped node into the stack (if it exists).
+// 4. Repeat the above steps #1 ~ #3, and we get the pre-order traversal effect.
 
 #include <vector>
+#include <stack>
 
 #include "BinaryTreeHelper.h";
 
@@ -32,9 +40,31 @@ public:
 	}
 };
 
-// Algorithm 2: (Recursive solution)
-class Solution2 {
+// Algorithm 2: (Iterative solution)
+class Solution {
 public:
 	vector<int> preorderTraversal(TreeNode* root) {
+		vector<int> res;
+		stack<TreeNode*> s;
+
+		if (!root) {
+			return res;
+		}
+
+		s.push(root);
+
+		while (!s.empty()) {
+			TreeNode* node = s.top();
+			s.pop();
+			res.push_back(node->val);
+			if (node->right) {
+				s.push(node->right);
+			}
+			if (node->left) {
+				s.push(node->left);
+			}
+		}
+
+		return res;
 	}
 };
